8ca15a8d46b1d21a231c7d999b8d787baed51866
MonsieurDa <azmout@gmail.com>
2022-01-19 11:49:09 +0100

    Add CI/CD in README.md

--- test.py
+++ test.py
@@ -2,18 +2,17 @@ import unittest
 
 from main import SurvivePredictor, PersonInformation
 
-
 class TestClassifier(unittest.TestCase):
     def test_classifier(self):
-        # One testcase to show, rather trivial
-        # We could test legal inputs, like non negatives etc
-        # But we did not implement
+        """a simple test implemented to test predictions of the classifier
+        """
         self.predictor = SurvivePredictor()
         assert 0.5 > self.predictor.predict(
             PersonInformation(sex='male', pclass=2)), "Issue with prediction negative"
         assert 0.5 < self.predictor.predict(
             PersonInformation(sex='female', pclass=2)), "Issue with prediction positive"
-
+    
+        
 
 if __name__ == '__main__':
     unittest.main()
